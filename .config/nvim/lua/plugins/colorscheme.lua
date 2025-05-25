4bfe000ba1c64ecfe0fbd930228e2559da1d94f1
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-05-25 16:18:32 -0500

    update colors neotree

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,5 +8,12 @@ return {
 		vim.g.gruvbox_material_enable_italic = true
 		vim.g.gruvbox_material_background = "hard"
 		vim.cmd.colorscheme("gruvbox-material")
+		-- Highlight para Neo-tree (gruvbox-material)
+		vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#1d2021" })
+		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#1d2021" })
+		-- Fondo del panel de estado y título
+		vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "#1d2021", fg = "#1d2021" }) -- elimina el símbolo `~`
+		vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "#1d2021", fg = "#1d2021" }) -- línea divisoria
+		vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { bg = "#32302f" }) -- línea bajo el cursor
 	end,
 }
