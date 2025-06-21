0ce81e904ad90fb9765edf13b224140ee1be1480
vgbrandon <villegas.galvan.brandon@gmail.com>
2025-06-21 00:12:19 -0500

    add misc plugin

---
+++ .config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,17 @@
+-- Highlight todo, notes, etc in comments
+return {
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
+}
+-- vim: ts=2 sts=2 sw=2 et
