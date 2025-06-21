83d1911990a9f5c13d9d69148029b03e7cd4ccb7
vgbrandon <villegas.galvan.brandon@gmail.com>
2025-06-21 00:11:55 -0500

    add gitsigns plugin

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,23 @@
+-- Adds git related signs to the gutter, as well as utilities for managing changes
+return {
+	"lewis6991/gitsigns.nvim",
+	opts = {
+		signs = {
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		signs_staged = {
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		numhl = false, -- Colorear numeros
+	},
+}
