f0345271038bbfdd65b697bba4c8258d05ec2c8f
vgbrandon <vgbrandon.dev@gmail.com>
2026-04-25 16:44:54 -0500

    feat(services): agregar instalador de paquetes pacman

---
+++ setup/services/pacman.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+install_pacman_packages() {
+  local packages=("$@")
+
+  if [ ${#packages[@]} -eq 0 ]; then
+    return
+  fi
+
+  sudo pacman -S --needed "${packages[@]}"
+}
