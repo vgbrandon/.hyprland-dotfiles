1e947b1c222e77a87d3e96d0cdf708dafce6c687
vgbrandon <vgbrandon.dev@gmail.com>
2026-05-03 15:18:55 -0500

    feat(stow): agregar configuración base de fish

---
+++ stow/fish/.config/fish/config.fish
@@ -0,0 +1,41 @@
+if status is-interactive
+    # Eliminando mensaje de saludo de Fish
+    function fish_greeting; end
+
+    # eza (better 'ls')
+    if type -q eza
+        alias l="eza --icons"
+        alias ls="eza --icons"
+        alias ll="eza -lg --icons"
+        alias la="eza -lag --icons"
+        alias lt="eza -lTg --icons"
+        alias lt1="eza -lTg --level=1 --icons"
+        alias lt2="eza -lTg --level=2 --icons"
+        alias lt3="eza -lTg --level=3 --icons"
+        alias lta="eza -lTag --icons"
+        alias lta1="eza -lTag --level=1 --icons"
+        alias lta2="eza -lTag --level=2 --icons"
+        alias lta3="eza -lTag --level=3 --icons"
+    end
+
+    # pnpm
+    set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+    fish_add_path -m "$PNPM_HOME"
+    # pnpm end
+
+    # Asegurar binarios locales
+    fish_add_path -m "$HOME/.local/bin"
+
+    if type -q zoxide
+        zoxide init fish | source
+    end
+
+    # opencode
+    fish_add_path -m "$HOME/.opencode/bin"
+
+    # Esto siempre debe ir al final
+    if type -q starship
+        set -gx STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
+        starship init fish | source
+    end
+end
